382456fe10edbc4dfca1ef5594bf0c1bd7775903
SrGiovanni <samuelefulghi@gmail.com>
2022-05-31 15:00:13 -0400

    add form reading to state

---
+++ src/components/Contact/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+function ContactForm() {
+  const [formState, setFormState] = useState( { name: '', email: '', message: '' } );
+
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    setFormState({...formState, name: e.target.value })
+
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  // JSX
+  return (
+      <section>
+        <h1>Contact me</h1>
+        <form id="contact-form">
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+          </div>
+          <div>
+            <label htmlFor="email" >Email address:</label>
+            <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea name="message" defaultValue={message} onChange={handleChange} rows="5"  />
+          </div>
+          <button type="submit" onSubmit={handleSubmit}>Submit</button>
+        </form>
+      </section>
+    )
+}
+    
+export default ContactForm;
